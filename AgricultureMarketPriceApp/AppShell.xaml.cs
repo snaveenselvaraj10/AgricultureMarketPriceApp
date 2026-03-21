8fcb575f0aca1548ff8de680e790c2925c5eee3e
snaveenselvaraj10 <snaveenselvaraj10@gmail.com>
2026-03-21 22:07:46 +0530

    Initial Commit

---
+++ AgricultureMarketPriceApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AgricultureMarketPriceApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
